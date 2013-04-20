26c834288d43c74b51104f8a2fdfe672a05a66b1
Ivan Bystrov <infernokernel@gmail.com>
2013-04-20 12:46:49 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Astrotool::Application.config.secret_token = 'c0561c17647e5a66461ec34608e9c40fa556a88164ba20ab517e67263eb9c317bfabbc66a8d17eed6b9e3f0410844c73f2a70998ff1f8dab29902160a32c6828'
